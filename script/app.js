3640d694d71c71d466be8febf9fb76a3f382c3d5
AnastaSAV <anastasia.personalbox@gmail.com>
2021-12-15 21:32:57 +0200

    first commit

---
+++ script/app.js
@@ -0,0 +1,25 @@
+
+	let string = 'Lorem ipsum dolor sit, amet suck consectetur adipisicing elit. Reiciendis dicta, vel fugiat beatae ipsum tenetur ullam, sint odio ducimus aliquam nesciunt dolor quibusdam ex ipsa provident fuck repellat non rerum saepe.';
+
+function stringMutant(firstWord, secondWord, string) {
+	string = string.split(' ');
+	console.log(string);
+	let newArray = [];
+	let consilience = false;
+	for(let i = 0; i < string.length; i++) {
+		if (string.includes(firstWord) && string.includes(secondWord)) {
+			consilience = true;
+			// console.log(array.indexOf(firstWord));
+			// console.log(array.indexOf(secondWord));
+		 string.splice(5, 1);
+		 string.splice (26, 1);
+		 newArray = string;
+		}
+		else {
+			consilience = false;
+		}
+  }
+  console.log(consilience);
+  console.log(newArray);
+}
+stringMutant('suck', 'fuck', string);
