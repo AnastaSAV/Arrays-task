97fa09be92b4c90b7d2349cf786570b96ad10d8b
AnastaSAV <anastasia.personalbox@gmail.com>
2021-12-17 14:27:49 +0200

    added function

--- script/app.js
+++ script/app.js
@@ -1,25 +1,36 @@
 
 	let string = 'Lorem ipsum dolor sit, amet suck consectetur adipisicing elit. Reiciendis dicta, vel fugiat beatae ipsum tenetur ullam, sint odio ducimus aliquam nesciunt dolor quibusdam ex ipsa provident fuck repellat non rerum saepe.';
-
-function stringMutant(firstWord, secondWord, string) {
+	
+function filterArray() {
 	string = string.split(' ');
 	console.log(string);
-	let newArray;
-	let consilience = false;
-	for(let i = 0; i < string.length; i++) {
-		if (string.includes(firstWord) && string.includes(secondWord)) {
-			consilience = true;
-			// console.log(array.indexOf(firstWord));
-			// console.log(array.indexOf(secondWord));
-		 string.splice(5, 1);
-		 string.splice (26, 1);
-		 newArray = (string.join(' '));
-		}
-		else {
-			consilience = false;
-		}
-  }
-  console.log(consilience);
-  console.log(newArray);
+	let filteredString = string.filter(function(item){
+		return item !== 'suck' && item !== 'fuck';
+	})
+	console.log(filteredString);
+	console.log(filteredString.join (' '));
 }
-stringMutant('suck', 'fuck', string);
+filterArray();
+
+// function stringMutant(firstWord, secondWord, string) {
+// 	string = string.split(' ');
+// 	console.log(string);
+// 	let newArray = [];
+// 	let consilience = false;
+// 	for(let i = 0; i < string.length; i++) {
+// 		if (string.includes(firstWord) && string.includes(secondWord)) {
+// 			consilience = true;
+// 			// console.log(array.indexOf(firstWord));
+// 			// console.log(array.indexOf(secondWord));
+// 		 string.splice(5, 1);
+// 		 string.splice (26, 1);
+// 		 newArray = (string.join(' '));
+// 		}
+// 		else {
+// 			consilience = false;
+// 		}
+//   }
+//   console.log(consilience);
+//   console.log(newArray);
+// }
+// stringMutant('suck', 'fuck', string);
